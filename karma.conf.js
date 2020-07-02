0c11e494bcaaed7b453d0b9a01d54533c227aff3
devopsarchitecture <devopsarchitecture@gmail.com>
2020-07-02 13:48:15 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -5,11 +5,10 @@ module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    browsers : ['Chrome', 'Firefox'],
+    browsers : ['Chrome'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
